871a19d4d9c4a69b1735f570f54b95ea30bc9181
Matias Piano <matias@globalfishingwatch.org>
2019-05-10 09:54:17 -0300

    Uses logging instead of logger

--- handlers/handle_incoming_email.py
+++ handlers/handle_incoming_email.py
@@ -4,12 +4,9 @@ from google.appengine.api.mail import Attachment
 from google.appengine.ext.webapp.mail_handlers import InboundMailHandler
 import webapp2
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-
 class VmsGCSUploaderHandler(InboundMailHandler):
     def receive(self, mail_message):
-        logger.info("Received a message from: " + mail_message.sender)
+        logging.info("Received a message from: " + mail_message.sender)
 
         plaintext_bodies = mail_message.bodies('text/plain')
         html_bodies = mail_message.bodies('text/html')
@@ -19,24 +16,25 @@ class VmsGCSUploaderHandler(InboundMailHandler):
 
         for body in html_bodies:
             decoded_html = body.decode()
-            logger.info("Html body of length %d.", len(decoded_html))
-            logger.info("Html body %s.", decoded_html)
+            logging.info("Html body of length %d.", len(decoded_html))
+            logging.info("Html body %s.", decoded_html)
 
         attachments = main_message.attachments
-        logger.info("Html attachments of length %d.", len(attachments))
+        logging.info("Html attachments of length %d.", len(attachments))
         for attachment in attachments:
-            logger.info("Attachment %s.", attachment)
+            logging.info("Attachment filename %s.", attachment.filename)
+            logging.info("Attachment payload %s.", attachment.payload)
             #Adds a unique identifier to the message YYYYMMDD-HHMM-HashOfTheMessageOfTheFile.data
             att_nam = msg_date + "-" + attachment + ".data"
-            logger.info("Attachment name %s.", att_name)
+            logging.info("Attachment name %s.", att_name)
             #Upload attachment to GCS
             # GCSTransfer()
 
         #TODO remove, just for debugging
         for content_type, body in plaintext_bodies:
             plaintext = body.decode()
-            logger.info("Plain text body of length %d.", len(plaintext))
-            logger.info("Plain text body %s.", plaintext)
+            logging.info("Plain text body of length %d.", len(plaintext))
+            logging.info("Plain text body %s.", plaintext)
 
 
 app = webapp2.WSGIApplication([VmsGCSUploaderHandler.mapping()], debug=True)
