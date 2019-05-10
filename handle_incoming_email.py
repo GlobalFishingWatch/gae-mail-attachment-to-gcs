4b7fb94b5ce500b1bd0e0b1941392e9580e36b4e
Matias Piano <matias@globalfishingwatch.org>
2019-05-10 15:13:30 -0300

    Reads the body in mail

--- handle_incoming_email.py
+++ handle_incoming_email.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import datetime
 
 from google.appengine.api.mail import Attachment
 from google.appengine.ext.webapp.mail_handlers import InboundMailHandler
@@ -8,28 +9,44 @@ import hashlib
 class VmsGCSUploaderHandler(InboundMailHandler):
     def receive(self, mail_message):
         logging.info("Received a message from: " + mail_message.sender)
+        logging.info("The email subject: " + mail_message.subject)
+        logging.info("The email was addressed to: " + str.join(str(mail_message.to), ', '))
+
+        try:
+            logging.info("The email was CC-ed to: " + str.join(str(mail_message.cc), ', '))
+        except exceptions.AttributeError:
+            logging.info("The email has no CC-ed recipients")
+
+        msg_date = datetime.now()
+        try:
+            msg_date = mail_message.date
+            logging.info("The email was send on: " + str(msg_date))
+        except exceptions.AttributeError :
+            logging.info("The email has no send date specified!!!")
 
         plaintext_bodies = mail_message.bodies('text/plain')
         html_bodies = mail_message.bodies('text/html')
 
         for body in html_bodies:
-            decoded_html, = body.decode()
+            logging.info("Html body %s.", body)
+            num, decoded_html = body
             logging.info("Html body of length %d.", len(decoded_html))
             logging.info("Html body %s.", decoded_html)
             logging.info("Html body tupple %d.", body.decode())
 
-        msg_date = mail_message.date
-        msg_date = msg_date.strftime("%Y%m%d-%H%M")
 
         attachments = main_message.attachments
         logging.info("Html attachments of length %d.", len(attachments))
         for attachment in attachments:
             logging.info("Attachment filename %s.", attachment.filename)
             logging.info("Attachment payload %s.", attachment.payload)
+
             hash_object = hashlib.md5(bytearray(attachment.content))
             attHash = hash_object.hexdigest()
+            msg_date_str = msg_date.strftime("%Y%m%d-%H%M")
+
             #Adds a unique identifier to the message YYYYMMDD-HHMM-HashOfTheMessageOfTheFile.data
-            att_nam = msg_date + "-" + attHash + ".data"
+            att_nam = msg_date_str + "-" + attHash + ".data"
             logging.info("Attachment name %s.", att_name)
             #Upload attachment to GCS
             # GCSTransfer()
