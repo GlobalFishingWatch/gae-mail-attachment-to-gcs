cb3ccb3b9bf584452378dbf5f700e7d87b95b322
Matias Piano <matias@globalfishingwatch.org>
2019-05-13 03:03:38 -0300

    Uploads with storage lib from google

--- utils/upload_gcs.py
+++ utils/upload_gcs.py
@@ -2,6 +2,7 @@ import ConfigParser
 import os
 import base64
 import logging
+from google.cloud import storage
 
 class GCSTransfer:
     def __init__(self):
@@ -19,15 +20,30 @@ class GCSTransfer:
 
         source_path = "/tmp/%s" % (source_file_name)
 
-        logging.info("Open to write the file %s".format(source_path))
+        logging.info("Open to write the file %s" % (source_path))
         f = open(source_path, 'wb')
         f.write(base64.b64decode(source_content))
         logging.info("File written")
         f.close()
 
+        upload_blob(source_path, "%s/%s" % (self.dir, source_file_name))
+
+    def upload_command(self, source_file_name, source_path)
         gcs_path = "gs://%s/%s/%s" % (self.bucket, self.dir, source_file_name)
         BOTO = "-o Boto:parallel_process_count=%s -o Boto:parallel_thread_count=%s" % (self.boto_process, self.boto_thread)
         command='gsutil -m -q %s cp %s %s' % (BOTO, source_path, gcs_path)
         print(command)
         os.system(command)
         logging.info("File %s upload to %s" % (source_file_name, gcs_path))
+
+    def upload_blob(self, source_path, destination_blob_name):
+        """Uploads a file to the bucket."""
+        storage_client = storage.Client()
+        bucket = storage_client.get_bucket(self.bucket)
+        blob = bucket.blob(destination_blob_name)
+
+        blob.upload_from_filename(source_path)
+
+        print('File {} uploaded to {}.'.format(
+            source_path,
+            destination_blob_name))
